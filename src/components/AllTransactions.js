8eb896fc40cb86043b1c65d4d4ef312cbdaedbd1
Pascal Birchler <hello@pascalbirchler.ch>
2017-04-26 17:12:17 +0200

    Begin with All Transactions view

--- src/components/AllTransactions.js
+++ src/components/AllTransactions.js
@@ -1,8 +1,10 @@
 // @flow
 
-import React from 'react'
+import React from 'react';
+import TransactionList from './TransactionList';
+import { getTransactions } from '../api';
+import type { User } from '../api';
 
-import type { User } from '../api'
 
 export type Props = {
   token: string,
@@ -11,13 +13,40 @@ export type Props = {
 
 class AllTransactions extends React.Component {
   
-  props: Props
-  
-  render() {    
-    return (
-      <div>All Transactions</div>
-    )
+  props: Props;
+
+  state: {
+    transactions: Transaction[],
+    error: Error
+  };
+
+  state = {
+    error: undefined
+  };
+
+  componentDidMount() {
+    getTransactions(this.props.token).then(result => {
+        this.setState({transactions: result.result});
+    })
+    .catch(error =>
+        this.setState({error})
+    );
+  }
+
+	render() {
+      return (
+        <div className="container">
+          <div className="columns">
+            <div className="column is-full">
+              <div className="box no-shadow">
+                <h2 className="title is-4">All Transactions</h2>
+                <TransactionList transactions={this.state.transactions} showDate="true" />
+              </div>
+            </div>
+          </div>
+        </div>
+      )
   }
 }
 
-export default AllTransactions
+export default AllTransactions;
