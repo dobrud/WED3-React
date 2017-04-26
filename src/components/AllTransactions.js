05916ca1154651cc0bda5a69b21cb4e3ca3f8d21
Pascal Birchler <hello@pascalbirchler.ch>
2017-04-26 17:53:09 +0200

    Styling for All Transactions

--- src/components/AllTransactions.js
+++ src/components/AllTransactions.js
@@ -1,10 +1,9 @@
 // @flow
 
-import React from 'react';
+import React from 'react'
 import TransactionList from './TransactionList';
 import { getTransactions } from '../api';
 import type { User } from '../api';
-
 
 export type Props = {
   token: string,
@@ -12,41 +11,46 @@ export type Props = {
 }
 
 class AllTransactions extends React.Component {
-  
-  props: Props;
 
-  state: {
-    transactions: Transaction[],
-    error: Error
-  };
+	props: Props;
 
-  state = {
-    error: undefined
-  };
+	state: {
+		transactions: Transaction[],
+		error: Error,
+	};
 
-  componentDidMount() {
-    getTransactions(this.props.token).then(result => {
-        this.setState({transactions: result.result});
-    })
-    .catch(error =>
-        this.setState({error})
-    );
-  }
+	state = {
+		error: undefined
+	};
+
+	componentDidMount() {
+		getTransactions( this.props.token ).then( result => {
+			this.setState( { transactions: result.result } );
+		} )
+		.catch( error =>
+			this.setState( { error } )
+		);
+	}
 
 	render() {
-      return (
-        <div className="container">
-          <div className="columns">
-            <div className="column is-full">
-              <div className="box no-shadow">
-                <h2 className="title is-4">All Transactions</h2>
-                <TransactionList transactions={this.state.transactions} showDate="true" />
-              </div>
-            </div>
-          </div>
-        </div>
-      )
-  }
+		return (
+			<div className="container">
+				<div className="columns">
+					<div className="column is-3">
+						<div className="box">
+							Filter
+						</div>
+			        </div>
+				    <div className="column is-9">
+				      <div className="box no-shadow">
+				        <h2 className="title is-4">All Transactions</h2>
+						<TransactionList transactions={this.state.transactions} showDate="true"/>
+				      </div>
+				    </div>
+			    </div>
+			</div>
+		)
+	}
 }
 
 export default AllTransactions;
